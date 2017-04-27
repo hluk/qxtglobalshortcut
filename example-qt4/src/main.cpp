1b4d5204b57c6e5500f4a76d105b5a6e303a7608
Lukas Holecek <hluk@email.cz>
2017-04-27 10:15:16 +0200

    Add Qt 4 example

---
+++ example-qt4/src/main.cpp
@@ -0,0 +1,30 @@
+#include <QApplication>
+
+#include <qxtglobalshortcut.h>
+
+#include <QTextStream>
+
+int main(int argc, char **argv)
+{
+    QApplication app(argc, argv);
+
+    QTextStream out(stdout);
+    QTextStream err(stderr);
+
+    const QKeySequence shortcut("Ctrl+Shift+T");
+    const QxtGlobalShortcut globalShortcut(shortcut);
+
+    if ( !globalShortcut.isValid() ) {
+        err << QString("Error: Failed to set shortcut %1")
+            .arg(shortcut.toString()) << endl;
+        return 1;
+    }
+
+    out << QString("Press shortcut %1 (or CTRL+C to exit)").arg(shortcut.toString()) << endl;
+
+    QObject::connect(
+                &globalShortcut, SIGNAL(activated(QxtGlobalShortcut*)),
+                &app, SLOT(quit()));
+
+    return app.exec();
+}
