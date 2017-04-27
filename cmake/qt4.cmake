5ca335dfb1e4d63fd1cf68618ef2359fd71576b5
Lukas Holecek <hluk@email.cz>
2017-04-27 10:11:33 +0200

    Fix Qt 4 build

--- cmake/qt4.cmake
+++ cmake/qt4.cmake
@@ -12,3 +12,7 @@ message(STATUS "Building with Qt 4.")
 include_directories(${QT_INCLUDES})
 add_definitions(${QT_DEFINITIONS})
 include(${QT_USE_FILE})
+target_link_libraries(${bin} ${QT_LIBRARIES})
+
+qt4_wrap_cpp(${bin}_mocced ${${bin}_moc})
+target_sources(${bin} PRIVATE ${${bin}_mocced})
